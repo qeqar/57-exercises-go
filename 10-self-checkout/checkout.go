fd514569f16857b5ed1ddcb3bfe38191ba2e45eb
Mark Eisenblaetter <mark.eisenblaetter@kaufhof.de>
2018-10-03 10:36:24 +0200

    Exercise 10 Self Checkout -> ok ok, the last commits works but........

--- 10-self-checkout/checkout.go
+++ 10-self-checkout/checkout.go
@@ -10,13 +10,9 @@ import (
 
 var taxRate = 0.55
 
-type Pair struct {
-	num, quantity interface{}
-}
-
 func main(){
 	reader := bufio.NewReader(os.Stdin)
-	var items [3]Pair //struct{num; quantity}
+	subTotal := 0.00
 
 	for i := 0; i < 3; i++ {
 		fmt.Printf("Enter the price of item %d: ", i + 1)
@@ -26,15 +22,9 @@ func main(){
 
 		num, _ := strconv.ParseFloat(strings.TrimSuffix(numRead, "\n"), 64)
 		quantity, _ := strconv.ParseFloat(strings.TrimSuffix(quantityRead, "\n"), 64)
-		m := Pair{num, quantity}
-		items[i] = m
+		subTotal += num * quantity
 	}
 
-	subTotal := 0.00
-	for i := range items {
-		subTotal += items[i].num.(float64) * items[i].quantity.(float64)
-
-	}
 	tax := subTotal * taxRate
 	total := subTotal + tax
 
