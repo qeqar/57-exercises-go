28211aec43d05749d7a6efc6f8dd71dd997b95d8
Mark Eisenblaetter <mark.eisenblaetter@kaufhof.de>
2018-11-04 12:02:46 +0100

    Exercise 12 Computing Simple Intrest

---
+++ 12-computing-simple-intrest/intrest.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the principal? ")
+	principalRead, _ := reader.ReadString('\n')
+	principal, _ := strconv.Atoi(strings.TrimSuffix(principalRead, "\n"))
+
+	fmt.Print("Enter the rate of intrest? ")
+	intrestRead, _ := reader.ReadString('\n')
+	intrestPercent, _:= strconv.Atoi(strings.TrimSuffix(intrestRead, "\n"))
+	intrest := float64(intrestPercent) / 100
+
+	fmt.Print("Enter the number of years? ")
+	yearsRead, _ := reader.ReadString('\n')
+	years, _ := strconv.Atoi(strings.TrimSuffix(yearsRead, "\n"))
+
+	amount := float64(principal) * (1 + (intrest * float64(years)))
+	fmt.Printf("After %d years at %df%, the investment will be worth %.2f€", years, intrestPercent,	 amount)
+
+}
